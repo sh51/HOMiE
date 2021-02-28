a57ef3aeb58f8b42c9fe18567cc0acd9e61135a6
Tyler Burnam <tyler@burnamtech.com>
2021-02-28 12:29:28 -0500

    added firebase dependicies

---
+++ app/src/main/java/com/cs65/homie/FirebaseHelper.java
@@ -0,0 +1,11 @@
+package com.cs65.homie;
+
+public class FirebaseHelper {
+
+    // Fetch account from ID
+    // Fetch profile
+    // Create profile
+    // Update profile
+    // Delete profile
+
+}
