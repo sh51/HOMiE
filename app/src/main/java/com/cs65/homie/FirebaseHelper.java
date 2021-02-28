7ae9d8ad92377e4211133121eac891887e8de0fa
Tyler Burnam <tyler@burnamtech.com>
2021-02-28 17:04:44 -0500

    added firebase dependencies and skeleton methods

--- app/src/main/java/com/cs65/homie/FirebaseHelper.java
+++ app/src/main/java/com/cs65/homie/FirebaseHelper.java
@@ -1,11 +1,30 @@
 package com.cs65.homie;
 
-public class FirebaseHelper {
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
 
+public class FirebaseHelper {
+    private FirebaseAuth mAuth;
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    // Auth endpoints
     // Fetch account from ID
     // Fetch profile
-    // Create profile
-    // Update profile
-    // Delete profile
 
+
+    // Create profile
+    public static void createProfile() {
+
+    }
+
+    // Update profile
+    public static void updateProfile() {
+
+    }
+
+
+    // Delete profile
+    public static void deleteProfile() {
+
+    }
 }
