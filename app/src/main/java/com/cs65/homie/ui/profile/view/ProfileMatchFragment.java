24a828a217e8a3f2861834bd72f6211621fbcbf4
Robert E. Price, III <42607775+REPIII@users.noreply.github.com>
2021-03-01 08:07:50 -0500

    Match profile view has unique fake data

--- app/src/main/java/com/cs65/homie/ui/profile/view/ProfileMatchFragment.java
+++ app/src/main/java/com/cs65/homie/ui/profile/view/ProfileMatchFragment.java
@@ -1,6 +1,7 @@
 package com.cs65.homie.ui.profile.view;
 
-import android.graphics.Color;
+import android.content.ContentResolver;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,9 +10,16 @@ import android.view.ViewGroup;
 import com.cs65.homie.MainActivity;
 import com.cs65.homie.R;
 import com.cs65.homie.Utilities;
+import com.google.android.gms.maps.model.LatLng;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.util.ArrayList;
+import java.util.List;
 
+
+/**
+ * Matching fragment, to handle matching functionality
+ */
 public class ProfileMatchFragment
     extends ProfileViewFragment
     implements View.OnClickListener
@@ -50,12 +58,14 @@ public class ProfileMatchFragment
         // FIXME Using fake data, need to set to the other user
         super.vm.setUserId(((MainActivity)this.getActivity()).getFakeUserId());
 
-        // The buttons require more padding
+        // The match buttons require more padding
         View topLayout = view.findViewById(R.id.profileViewLayout);
         if (topLayout != null)
         {
-            int padding = (int)Math.round(Utilities.pixelDensity(this.getContext(), 80.0));
-            topLayout.setPadding(0, 0, 0, padding);
+            topLayout.setPadding(
+                0, 0, 0,
+                (int)Math.round(Utilities.pixelDensity(this.getContext(), 80.0))
+            );
         }
 
         // Set up the listener for the match buttons
@@ -74,6 +84,44 @@ public class ProfileMatchFragment
             buttonReject.setOnClickListener(this);
             buttonReject.setVisibility(View.VISIBLE);
         }
+        // TODO Using fake data for now
+        this.loadFakeData();
+
+    }
+
+    /**
+     * Load fake data in lieu of Firebase, for testing
+     */
+    private void loadFakeData()
+    {
+
+        this.vm.getBathroom().setValue(true);
+        this.vm.getBio().setValue(
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do "
+                + "eiusmod tempor incididunt ut labore et dolore magna aliqua. "
+                + "Ut enim ad minim veniam, quis nostrud exercitation ullamco "
+                + "laboris nisi ut aliquip ex ea commodo consequat. Duis aute "
+                + "irure dolor in reprehenderit in voluptate velit esse cillum "
+                + "dolore eu fugiat nulla pariatur. Excepteur sint occaecat "
+                + "cupidatat non proident, sunt in culpa qui officia deserunt "
+                + "mollit anim id est laborum."
+        );
+        this.vm.getGender().setValue("Female");
+        this.vm.getLoc().setValue(new LatLng(43.624794, -72.323171));
+        this.vm.getMyLoc().setValue(new LatLng(43.704166, -72.288762));
+        this.vm.getMyLocLive().setValue(true);
+        this.vm.getMyLocStr().setValue("Sanborn");
+        this.vm.getPets().setValue(true);
+        this.vm.getPlace().setValue(true);
+        this.vm.getProfileName().setValue("Jane");
+        this.vm.getSmoking().setValue(true);
+
+        List<Uri> images = new ArrayList<Uri>();
+        images.add(Uri.parse(ContentResolver.SCHEME_ANDROID_RESOURCE + "://com.cs65.homie/" + R.drawable.dart0));
+        images.add(Uri.parse(ContentResolver.SCHEME_ANDROID_RESOURCE + "://com.cs65.homie/" + R.drawable.dart1));
+        images.add(Uri.parse(ContentResolver.SCHEME_ANDROID_RESOURCE + "://com.cs65.homie/" + R.drawable.dart2));
+        images.add(Uri.parse(ContentResolver.SCHEME_ANDROID_RESOURCE + "://com.cs65.homie/" + R.drawable.dart3));
+        this.vm.getimages().setValue(images);
 
     }
 
