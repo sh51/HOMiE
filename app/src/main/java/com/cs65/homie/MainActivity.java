3c76a5a96e846b45de3215ae32e1963f4c1395ee
Robert E. Price, III <42607775+REPIII@users.noreply.github.com>
2021-03-01 06:07:21 -0500

    Profile view moved to fragment

--- app/src/main/java/com/cs65/homie/MainActivity.java
+++ app/src/main/java/com/cs65/homie/MainActivity.java
@@ -3,9 +3,7 @@ package com.cs65.homie;
 import android.content.Intent;
 import android.os.Bundle;
 
-import com.cs65.homie.ui.ProfileSettingsActivity;
 import com.cs65.homie.ui.login.ui.login.LoginActivity;
-import com.cs65.homie.ui.profile.view.ProfileViewActivity;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,18 +18,41 @@ public class MainActivity extends AppCompatActivity {
     // name "homie" has too many matches in regex logging mode
     public static final String TAG = "HOMIES";
 
+    // Fake app user ID for testing/demoing before Firebase
+    // Profile view needs it
+    // This eventually needs to be saved to the device somewhere for profile
+    // view to operate
+    public long getFakeMyId()
+    {
+        return 42L;
+    }
+    // Fake profile user ID for testing/demoing before Firebase
+    // Profile view needs it
+    public long getFakeUserId()
+    {
+        return 41L;
+    }
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_messages, R.id.navigation_dashboard, R.id.navigation_notifications)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+                R.id.navigation_messages,
+                R.id.navigation_dashboard,
+                R.id.navigation_profile
+        ).build();
+        NavController navController = Navigation.findNavController(
+            this, R.id.nav_host_fragment
+        );
+        NavigationUI.setupActionBarWithNavController(
+            this, navController, appBarConfiguration
+        );
         NavigationUI.setupWithNavController(navView, navController);
 
         Intent intent = new Intent(this, LoginActivity.class);
