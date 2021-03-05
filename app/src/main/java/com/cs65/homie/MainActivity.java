52ba9c5c549f9c0d4ceb896827be19dd1d67e63f
Robert E. Price, III <42607775+REPIII@users.noreply.github.com>
2021-03-05 12:43:49 -0500

    Restore login activity spawn

--- app/src/main/java/com/cs65/homie/MainActivity.java
+++ app/src/main/java/com/cs65/homie/MainActivity.java
@@ -105,9 +105,8 @@ public class MainActivity extends AppCompatActivity {
         );
         NavigationUI.setupWithNavController(navView, navController);
 
-        // TODO Don't forge to restore this REPIII
-        //Intent intent = new Intent(this, LoginActivity.class);
-        //startActivityForResult(intent, RC_LOGIN);
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivityForResult(intent, RC_LOGIN);
 
         // TODO The landing activity should probably be profile?
         // It shouldn't be messages at least?
