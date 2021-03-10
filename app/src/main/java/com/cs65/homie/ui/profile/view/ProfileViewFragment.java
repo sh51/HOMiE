8a4661282af672f3938e2bb35c6fbf1ffa3c162a
sh <huang.0x51@gmail.com>
2021-03-10 13:13:46 -0500

    added image carousel fake data

--- app/src/main/java/com/cs65/homie/ui/profile/view/ProfileViewFragment.java
+++ app/src/main/java/com/cs65/homie/ui/profile/view/ProfileViewFragment.java
@@ -156,7 +156,7 @@ public class ProfileViewFragment extends Fragment
     )
     {
 
-//        this.loadFakeData();
+        this.loadFakeData();
 
         // Load the layout
         return inflater.inflate(
